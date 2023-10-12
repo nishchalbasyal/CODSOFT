0efa3d81da8556335941fc1dc06615fda1f7fd13
Nishchal Basyal <nischalbasyal2003@gmail.com>
2023-10-13 00:18:47 +0530

    change to sqlite database

--- blogging_platform/settings.py
+++ blogging_platform/settings.py
@@ -135,18 +135,18 @@ WSGI_APPLICATION = 'blogging_platform.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': config('DB_NAME'),  # Replace with your database name
-         'USER': config('DB_USER'),               # Default MySQL user in XAMPP
-        'PASSWORD': config('DB_PASSWORD'),               # Leave empty if no password set
-        'HOST': config('DB_HOST'),          # MySQL server host (localhost)
-        'PORT': config('DB_PORT'),              # Default MySQL port
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': config('DB_NAME'),  # Replace with your database name
+    #      'USER': config('DB_USER'),               # Default MySQL user in XAMPP
+    #     'PASSWORD': config('DB_PASSWORD'),               # Leave empty if no password set
+    #     'HOST': config('DB_HOST'),          # MySQL server host (localhost)
+    #     'PORT': config('DB_PORT'),              # Default MySQL port
+    # }
 }
 
 
